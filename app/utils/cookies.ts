ebfe98e9066c976112dcf9da60493e891c0a9589
Onarbf <onarbf@gmail.com>
2023-11-14 16:57:11 +0100

    --

--- app/utils/cookies.ts
+++ app/utils/cookies.ts
@@ -2,9 +2,10 @@ export const getCookie = (name:string)=>{
     if (typeof window !== "undefined") {
         const value = `; ${document.cookie}`;
         const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
+        if (parts.length === 2) return parts.pop()?.split(';').shift();
         }
     
+    return 'default';
   }
 
 
