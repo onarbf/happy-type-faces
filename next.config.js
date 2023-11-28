b21273d1db1d357b37d069c6fb1be1729535ba31
Onarbf <onarbf@gmail.com>
2023-11-28 12:27:58 +0100

    --

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
 const withNextIntl = require('next-intl/plugin')();
  
-module.exports = {
-    async headers () {
-        return [
-          {
-            // Apply these headers to all routes in your application.
-            source: '/:path*',
-            headers: [
-              {
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors 'self' https://onar.dev;"
-              },
-              {
-                key: 'X-Frame-Options',
-                value: 'ALLOW-FROM https://onar.dev'
-              }
-            ]
-          }
-        ]
-      }
-}
+module.exports = withNextIntl({
+
+});
