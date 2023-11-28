7f3fc1923355b1916ee5e968a78e066c19bcf280
Onarbf <onarbf@gmail.com>
2023-11-28 22:02:02 +0100

    --

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -2,16 +2,14 @@
 
 import themes from "@/app/constants/themes";
 import { useGlobalContext } from "@/app/context/store";
-import { Suspense, useState } from "react";
+import { useState } from "react";
 import SideBar from "../sidebar";
 import Image from "next/image";
 
 export default function Header(){
     const [isOpen, setIsOpen] = useState(false);
     const {theme} = useGlobalContext();
-    return(
-    <Suspense >
-        <header className={`h-[6vh] px-[2.5vw] pt-[2.5vh] text-[1.3rem] font-bold ${themes[theme!].textColor}`}>
+    return(<header className={`h-[6vh] px-[2.5vw] pt-[2.5vh] text-[1.3rem] font-bold ${themes[theme!] ? themes[theme!].textColor: 'text-pinky'}`}>
             <div className="flex items-center justify-between">
                 <div className="">
                 <Image alt="change_theme" src={'/media/logo.svg'} className="h-auto w-[200px] md:w-[300px]"  width={300} height={300} />
@@ -30,6 +28,5 @@ export default function Header(){
                 <div onClick={() => setIsOpen(false)}  className={`relative z-10 ${themes[theme!].bgColor}`}>
                 {isOpen && (<SideBar/>)}
             </div>
-        </header>
-        </Suspense>)
+        </header>)
 }
